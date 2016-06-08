a578a301901cb694e9688025b85f69f8cecd886d
Jonathan Bossenger <jonathanbossenger@gmail.com>
2016-06-08 22:44:44 +0200

    Initial commit

---
+++ aw-bloom-redirection.php
@@ -0,0 +1,142 @@
+<?php
+/*
+ * Plugin Name: AW Bloom Redirection
+ * Version: 1.0
+ * Plugin URI: http://jonathanbossenger.com/
+ * Description: This is an empty shell for your next WordPress plugin.
+ * Author: Jonathan Bossenger
+ * Author URI: http://jonathanbossenger.com/
+ * Requires at least: 4.0
+ * Tested up to: 4.0
+ *
+ * Text Domain: aw-bloom-redirection
+ * Domain Path: /lang/
+ *
+ * @package WordPress
+ * @author Jonathan Bossenger
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// do your stuff here
+
+// add bloom sub menu item
+// bloom redirection form, lists all current optins
+// edit optin allows you to set a redirect url
+
+define( 'AW_BLOOM_REDIRECTION_PLUGIN_DIR', trailingslashit( dirname(__FILE__) ) );
+define( 'AW_BLOOM_REDIRECTION_PLUGIN_URI', plugins_url('', __FILE__) );
+
+/* enqueue scripts */
+function aw_enqueue_scripts() {
+	/*wp_dequeue_script( 'et_bloom-custom-js' );
+	wp_enqueue_script( 'aw_bloom-custom-js', AW_BLOOM_REDIRECTION_PLUGIN_URI . '/js/custom.js', array( 'jquery' ) );
+	$protocol = is_ssl() ? 'https' : 'http';
+	wp_localize_script( 'aw_bloom-custom-js', 'bloomSettings', array(
+		'ajaxurl'         => admin_url( 'admin-ajax.php', $protocol ),
+		'pageurl'         => ( is_singular( get_post_types() ) ? get_permalink() : '' ),
+		'stats_nonce'     => wp_create_nonce( 'update_stats' ),
+		'subscribe_nonce' => wp_create_nonce( 'subscribe' ),
+	) );
+	*/
+	$aw_enable_bloom_redirection = get_option( 'aw_enable_bloom_redirection', '' );
+	if ( 'on' == $aw_enable_bloom_redirection ){
+		$protocol = is_ssl() ? 'https' : 'http';
+
+		wp_enqueue_script( 'functions', AW_BLOOM_REDIRECTION_PLUGIN_URI . '/js/functions.js', array(), false, true );
+		$aw_bloom_redirections = get_option( 'aw_bloom_redirection', array() );
+
+		wp_localize_script( 'functions', 'bloomRedirectionSettings', array(
+			'redirections' 	=> $aw_bloom_redirections,
+			'site_url' 		=> site_url(),
+			'ajaxurl'       => admin_url( 'admin-ajax.php', $protocol )
+		) );
+	}
+}
+
+add_action('wp_enqueue_scripts', 'aw_enqueue_scripts', 99);
+
+function aw_add_bloom_redirection_submenu(){
+	add_menu_page( esc_html__( 'Bloom Redirects', 'bloom_redirection' ), esc_html__( 'Bloom Redirects', 'bloom_redirection' ), 'manage_options', 'aw_bloom_redirection_editor', 'aw_display_bloom_redirection_editor', 'dashicons-migrate' );
+	//add_submenu_page( 'et_bloom_redirects', esc_html__( 'Set URLS', 'bloom_redirection' ), esc_html__( 'Set URLS', 'bloom_redirection' ), 'manage_options', 'aw_bloom_redirection_editor', 'aw_display_bloom_redirection_editor' );
+}
+add_action('admin_menu', 'aw_add_bloom_redirection_submenu', 11);
+
+function aw_display_bloom_redirection_editor() {
+
+	if ( !current_user_can( 'manage_options' ) ) {
+		wp_die( 'You are not allowed to perform this action.' );
+	}
+
+	if ( isset( $_POST['aw_bloom_redirection_editor'] ) && wp_verify_nonce( $_POST['aw_bloom_redirection_editor'], 'aw_display_bloom_redirection_editor' ) ) {
+		if ( isset ( $_REQUEST ) && !empty ( $_REQUEST ) && isset( $_REQUEST['action'] ) && !empty ( $_REQUEST['action'] ) && $_REQUEST['action'] == 'asu_social_media_update' ) {
+			$bloom_redirection = $_REQUEST['bloom_redirection'];
+			update_option( 'aw_bloom_redirection', $bloom_redirection );
+			if ( isset( $_REQUEST['enable_bloom_redirection'] ) && 'on' == $_REQUEST['enable_bloom_redirection'] ){
+				update_option( 'aw_enable_bloom_redirection', $_REQUEST['enable_bloom_redirection'] );
+			}else {
+				update_option( 'aw_enable_bloom_redirection', '' );
+			}
+			echo '<div id="message" class="updated fade"><p><strong>Bloom redirection urls saved.</strong></p></div>';
+		}
+	} else {
+		// show some error message here?
+	}
+
+	$aw_enable_bloom_redirection = get_option( 'aw_enable_bloom_redirection', '' );
+	$aw_bloom_redirections = get_option( 'aw_bloom_redirection', array() );
+
+	$et_bloom_options = get_option( 'et_bloom_options', array() );
+
+	$bloom_optins = array();
+	foreach ($et_bloom_options as $key => $value){
+		$is_bloom_optin = strpos($key, 'optin');
+		if ($is_bloom_optin === false) {
+			continue;
+		} else {
+			$bloom_optins[$key] = $value;
+		}
+	}
+
+	?>
+	<div class="wrap" id="aw_bloom_redirection_editor">
+		<h2>Edit Bloom optin redirects</h2>
+		<form method="post" action="admin.php?page=aw_bloom_redirection_editor" enctype="multipart/form-data">
+			<input type="hidden" name="action" value="asu_social_media_update">
+			<?php wp_nonce_field('aw_display_bloom_redirection_editor', 'aw_bloom_redirection_editor'); ?>
+			<h2>Bloom options</h2>
+			<p>Add a redirection url for your Bloom optins.</p>
+			<table class="form-table">
+				<tbody>
+				<tr>
+					<th scope="row">Enable redirection</th>
+					<td><input id="enable_bloom_redirection" type="checkbox" name="enable_bloom_redirection" value="on" <?php if ('on' == $aw_enable_bloom_redirection) { echo 'checked="checked"'; } ?>>
+						<label for="text_field">
+							<span class="description">Enable or disable redirection. If redirection is disabled, the standard Bloom subscribe action fires.</span>
+						</label>
+					</td>
+				</tr>
+
+				<?php foreach ( $bloom_optins as $option_key => $optin ) { ?>
+				<tr>
+					<th scope="row"><?php echo $optin['optin_name'] ?></th>
+					<td><input id="bloom_redirection_<?php echo $option_key ?>" type="text" name="bloom_redirection[<?php echo $option_key ?>]" value="<?php echo ( isset( $aw_bloom_redirections[$option_key] ) ) ? $aw_bloom_redirections[$option_key] : ''; ?>">
+						<label for="text_field">
+							<span class="description">Enter the Redirection URL for <?php echo $optin['optin_name'] ?>.</span>
+						</label>
+					</td>
+				</tr>
+				<?php } ?>
+
+				</tbody>
+			</table>
+			<p class="submit">
+				<input type="hidden" name="tab" value="">
+				<input name="Submit" type="submit" class="button-primary" value="Save Settings">
+			</p>
+		</form>
+	</div>
+	<?php
+}
+
---
+++ js/functions.js
@@ -0,0 +1,112 @@
+jQuery(document).ready(function(){
+    console.log('loaded');
+    console.log(bloomRedirections);
+
+    jQuery( '.et_bloom_submit_subscription' ).each(function( index ) {
+        jQuery( this ).parents('form').on('submit', function(event){
+            return false;
+        });
+    });
+
+    /*
+    jQuery( 'body' ).on( 'click', '.et_bloom_locked_container .et_bloom_submit_subscription', function(){
+        var current_container = $( this ).closest( '.et_bloom_locked_container' ),
+            container_id = current_container.data( 'container_id' ),
+            page_id = current_container.data( 'page_id' ),
+            optin_id = current_container.data( 'optin_id' );
+
+        aw_perform_subscription( $( this ), current_container, container_id, page_id, optin_id );
+
+        return false;
+    });
+
+    jQuery( 'body' ).on( 'click', '.et_bloom_submit_subscription:not(.et_bloom_submit_subscription_locked)', function() {
+        perform_subscription( $( this ), '', '', '', '' );
+        return false;
+    });
+    */
+    
+    jQuery('.et_bloom_submit_subscription').on('click', function(event){
+        event.preventDefault();
+        if ( !jQuery( this ).hasClass('et_bloom_submit_subscription_locked') ){
+            aw_perform_subscription( jQuery( this ), '', '', '', '' );
+        }else {
+            var current_container = jQuery( this ).closest( '.et_bloom_locked_container' ),
+                container_id = current_container.data( 'container_id' ),
+                page_id = current_container.data( 'page_id' ),
+                optin_id = current_container.data( 'optin_id' );
+
+            aw_perform_subscription( $( this ), current_container, container_id, page_id, optin_id );
+        }
+        return false;
+    });
+
+    function aw_perform_subscription( this_button, current_container, container_id, locked_page_id, locked_optin_id ) {
+        var this_form = this_button.parent(),
+            list_id = this_button.data( 'list_id' ),
+            account_name = this_button.data( 'account' ),
+            service = this_button.data( 'service' ),
+            name = this_form.find( '.et_bloom_subscribe_name input' ).val(),
+            last_name = undefined != this_form.find( '.et_bloom_subscribe_last input' ).val() ? this_form.find( '.et_bloom_subscribe_last input' ).val() : '',
+            email = this_form.find( '.et_bloom_subscribe_email input' ).val(),
+            page_id = this_button.data( 'page_id' ),
+            optin_id = this_button.data( 'optin_id' ),
+            disable_dbl_optin = this_button.data( 'disable_dbl_optin' );
+
+        this_form.find( '.et_bloom_subscribe_email input' ).removeClass( 'et_bloom_warn_field' );
+
+        if ( '' == email ) {
+            this_form.find( '.et_bloom_subscribe_email input' ).addClass( 'et_bloom_warn_field' );
+        } else {
+            $subscribe_data = JSON.stringify({ 'list_id' : list_id, 'account_name' : account_name, 'service' : service, 'name' : name, 'email' : email, 'page_id' : page_id, 'optin_id' : optin_id, 'last_name' : last_name, 'dbl_optin' : disable_dbl_optin });
+            jQuery.ajax({
+                type: 'POST',
+                dataType: 'json',
+                url: bloomSettings.ajaxurl,
+                data: {
+                    action : 'bloom_subscribe',
+                    subscribe_data_array : $subscribe_data,
+                    subscribe_nonce : bloomSettings.subscribe_nonce
+                },
+                beforeSend: function( data ) {
+                    this_button.addClass( 'et_bloom_button_text_loading' );
+                    this_button.find( '.et_bloom_subscribe_loader' ).css( { 'display' : 'block' } );
+                },
+                success: function( data ) {
+                    this_button.removeClass( 'et_bloom_button_text_loading' );
+                    this_button.find( '.et_bloom_subscribe_loader' ).css( { 'display' : 'none' } );
+                    if ( data ) {
+                        if ( '' != current_container && ( data.success || 'Invalid email' != data.error ) ) {
+                            unlock_content( current_container, container_id, locked_page_id, locked_optin_id );
+                        } else {
+                            if ( data.error ) {
+                                this_form.find( '.et_bloom_error_message' ).remove();
+                                this_form.prepend( '<h2 class="et_bloom_error_message">' + data.error + '</h2>' );
+                                this_form.parent().parent().find( '.et_bloom_form_header' ).addClass( 'et_bloom_with_error' );
+                            }
+                            if ( data.success && '' == current_container ) {
+                                var redirectURL = '';
+                                bloomRedirections = bloomRedirectionSettings['redirections'];
+                                if (optin_id in bloomRedirections){
+                                    redirectURL = bloomRedirections[optin_id];
+                                }else {
+                                    redirectURL = bloomRedirectionSettings['site_url'];
+                                }
+                                window.location.href = redirectURL;
+                                /*
+                                this_form.parent().find( '.et_bloom_success_message' ).addClass( 'et_bloom_animate_message' );
+                                this_form.parent().find( '.et_bloom_success_container' ).addClass( 'et_bloom_animate_success' );
+                                this_form.remove();
+                                set_cookie( 365, 'et_bloom_subscribed_to_' + optin_id + list_id + '=true' );
+                                */
+                            }
+                        }
+
+                        define_popup_position( this_form.parent().parent().parent().parent(), false, 50 );
+                    }
+                }
+            });
+        }
+    }
+});
+
